1eda7a66f552172ee94b663232d628491d719775
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-05-17 19:15:56 +0900

    Can select center

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,6 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description="Calculate the Mandelbrot set (500 iterations)")
 parser.add_argument('-p', '--precision', default=0.1, type=float, help="Precision of the 'net'")
 parser.add_argument('-n', '--max', default=100, type=int, help="Number of iterations")
+parser.add_argument('-r', '--centerreal', default=0, type=float, help="Real part of figure center")
+parser.add_argument('-i', '--centerimag', default=0, type=float, help="Imaginary part of figure center")
 args = parser.parse_args()
 
 
@@ -12,6 +14,8 @@ from complex import Complex
 
 MAX = args.max
 OUT_OF_MANDELBROT = 2
+CENTER = Complex(args.centerreal, args.centerimag)
+USE_SYMMETRY = CENTER.i == 0
 
 def f(c, z):
     return z*z + c
@@ -31,16 +35,21 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import tweakmatplotlib
 
-SIZE = int(4/PRECISION)//2*2
+if USE_SYMMETRY:
+    SIZE = int(4/PRECISION)//2*2
+else:
+    SIZE = int(4/PRECISION)
+
 out = np.zeros((SIZE, SIZE), dtype=int)
 
 for i in range(0, SIZE):
     print("%d/%d\r" % (i,SIZE), end="")
-    for j in range(0, SIZE//2+1):
-        c = Complex(-2+i*PRECISION, -2+j*PRECISION)
+    RANGE = SIZE//2+1 if USE_SYMMETRY else SIZE
+    for j in range(0, RANGE):
+        c = Complex(CENTER.r-2+i*PRECISION, CENTER.i-2+j*PRECISION)
         out[j,i] = isInside(c)
     # Use symmetry of set to gain time
-    for j in range(SIZE//2+1, SIZE):
+    for j in range(RANGE, SIZE):
         out[j,i] = out[SIZE-j, i]
 print("%d/%d" % (SIZE,SIZE))
 np.savetxt("out.csv", out, fmt="%d", delimiter=",")
