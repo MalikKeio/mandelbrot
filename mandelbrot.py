e98c06821fd97b6ebb00a81ab82187c0bc59133f
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-05-17 18:51:46 +0900

    Use symmetry to computer quicker

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Calculate the Mandelbrot set (500 iterations)")
 parser.add_argument('-p', '--precision', default=0.1, type=float, help="Precision of the 'net'")
-parser.add_argument('-n', '--max', default=500, type=int, help="Number of iterations")
+parser.add_argument('-n', '--max', default=100, type=int, help="Number of iterations")
 args = parser.parse_args()
 
 
@@ -31,14 +31,17 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import tweakmatplotlib
 
-SIZE = int(4/PRECISION)
+SIZE = int(4/PRECISION)//2*2
 out = np.zeros((SIZE, SIZE), dtype=int)
 
 for i in range(0, SIZE):
     print("%d/%d\r" % (i,SIZE), end="")
-    for j in range(0, SIZE):
+    for j in range(0, SIZE//2+1):
         c = Complex(-2+i*PRECISION, -2+j*PRECISION)
         out[j,i] = isInside(c)
+    # Use symmetry of set to gain time
+    for j in range(SIZE//2+1, SIZE):
+        out[j,i] = out[SIZE-j, i]
 print("%d/%d" % (SIZE,SIZE))
 np.savetxt("out.csv", out, delimiter=",")
 plt.imshow(out)
